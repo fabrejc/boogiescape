3214b5389088ae9826610c331cc3ca7956fa64a0
Jean-Christophe Fabre <jean-christophe.fabre@inra.fr>
2019-05-24 12:11:44 +0200

    Switch to version 1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name = 'boogiescape',
-      version = '0.0.1',
+      version = '1.0.0',
       description = 'Tool for adjusting spatial representations of agricultural landscapes for the OpenFLUID modelling platform',
       long_description = readme(),
       author = 'Jean-Christophe Fabre',
